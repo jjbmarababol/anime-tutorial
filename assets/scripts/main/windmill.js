caf9843799922791f9c4d56b39ae9bc2b331c488
jjbmarababol <jigs@coinspectapp.com>
2020-11-30 12:22:10 +0800

    [AT-5] Specific transform origin and different anime duration

---
+++ assets/scripts/main/windmill.js
@@ -0,0 +1,16 @@
+
+anime({
+  targets: ['#aermotor', "#turbine"],
+  rotate: 360,
+  loop: true,
+  easing: 'linear',
+  duration: 2000,
+});
+
+anime({
+  targets: ['#mill'],
+  rotate: 360,
+  loop: true,
+  easing: 'linear',
+  duration: 5000,
+})
--- assets/scripts/windmill.js
+++
@@ -1,8 +0,0 @@
-
-anime({
-  targets: ['#aermotor', '#mill'],
-  rotate: 360,
-  easing: 'linear',
-  loop: true,
-  duration: 2000,
-});
